bd3f848aa8ac03a2c99bbe612d2329ad4b338177
Noorsat <itsnursat@gmail.com>
2023-06-19 05:59:25 +0600

    f

--- index.js
+++ index.js
@@ -23,10 +23,34 @@ socketIO.on('connection', (socket) => {
     socket.on('selectSeance', (data) => {
         socketIO.emit('selectSeanceResponse', data);
     })
-
+    
     socket.on('lockSeat', (data) => {
         socketIO.emit('lockSeatResponse', data)
     })
+
+    socket.on('buy', (data) => [
+        socketIO.emit('buyResponse', data)
+    ])
+
+    socket.on('reserve', (data) => [
+        socketIO.emit('reserveResponse', data)
+    ])
+
+    socket.on('unlock', (data) => [
+        socketIO.emit('unlockResponse', data)
+    ])
+    
+    socket.on('unlockAll', (data) => [
+        socketIO.emit('unlockAllResponse', data)
+    ])
+    
+    socket.on('returnTicket', (data) => [
+        socketIO.emit('returnTicketResponse', data)
+    ])
+
+    socket.on('toMainPage', (data) => [
+        socketIO.emit('toMainPageResponse', data)
+    ])
 
     socket.on('disconnect', () => {
         console.log(`user ${socket.id} disconnect`)
